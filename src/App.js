f5bdc765aee224c2949074a6e40f78c5397b7c9f
Stone <swestray@gmail.com>
2017-03-16 19:04:47 -0400

    hi jon

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Router, Route, hashHistory } from 'react-router';
+import Tiger from './routeComponents/tiger';
 import './App.css';
 
-const Home = <h1>Home</h1>
-
-const Tiger = <h1>Tiger</h1>
-
-var App = React.createClass({
+class App extends Component{
   render() {
     return (
       <Router history={hashHistory}>
-        <Route path='/' component={Home}/>
-        <Route path='/tiger' component={Tiger} />
+        <Route path='/' component={Tiger}/>
       </Router>
     );
   }
-})
+}
 
-module.exports = App;
+export default App
---
+++ src/routeComponents/tiger.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Tiger extends Component{
+	render(){
+		return <h1> hey Bitch </h1>
+	}
+}
+
+export default Tiger;
